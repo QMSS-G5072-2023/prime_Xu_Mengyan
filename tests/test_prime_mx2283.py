0630d67e2ee43fe52025b556557d221e1eb1201b
mengyan0623 <143971207+mengyan0623@users.noreply.github.com>
2023-10-30 19:19:34 -0400

    Added tests and updated dependencies

--- tests/test_prime_mx2283.py
+++ tests/test_prime_mx2283.py
@@ -1,1 +1,24 @@
-from prime_mx2283 import prime_mx2283
+from prime_mx2283 import is_prime
+
+# +
+import pytest
+
+def test_is_prime():
+    # Test prime numbers
+    assert is_prime(2) == True, "2 should be prime"
+    assert is_prime(7) == True, "7 should be prime"
+    assert is_prime(11) == True, "11 should be prime"
+    
+    # Test non-prime numbers
+    assert is_prime(1) == False, "1 is not prime"
+    assert is_prime(8) == False, "8 should not be prime"
+    assert is_prime(9) == False, "9 should not be prime"
+    
+    # Test with zero and negative number
+    assert is_prime(0) == False, "0 is not prime"
+    assert is_prime(-7) == False, "-7 should not be prime, only positive integers can be prime"
+# -
+
+# !pytest test_prime_mx2283.py
+
+
